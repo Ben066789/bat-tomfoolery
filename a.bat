7e2bfeb392364d72761248df0c66fc9bef67249d
Ben Piñon <dabenz8293@gmail.com>
2026-03-19 22:36:47 +0800

    wait for Z mount

--- a.bat
+++ a.bat
@@ -1,16 +1,35 @@
-@echo off 
-echo grabbing flash drive letter... 
-echo. 
-set "flshlttr=%~d0" 
-timeout /t 3 >nul 
-echo drive letter is "%flshlttr%"... 
-echo. 
-echo starting veracrypt... 
-timeout /t 3 >nul 
-cd /d "%flshlttr%\veracrypt" 
-vc /v "%flshlttr%\drives\rgd_flgn" /l Z /a /q 
-echo. 
-set /p choice="firefox? (y/n): " 
-if /i "%choice%"=="y" 
-(echo. start "" "Z:\FirefoxPortable\FirefoxPortable.exe") 
-else ( echo. echo aight bro timeout /t 3 >nul )
+@echo off
+
+echo grabbing flash drive letter...
+echo.
+
+set "flshlttr=%~d0"
+
+timeout /t 2 >nul
+
+echo drive letter is "%flshlttr%"...
+echo.
+
+echo starting veracrypt...
+timeout /t 2 >nul
+
+cd /d "%flshlttr%\veracrypt"
+
+"%flshlttr%\veracrypt\vc.exe" /v "%flshlttr%\drives\rgd_flgn" /l Z /a /q
+
+if not exist Z:\ (
+    timeout /t 1 >nul
+    goto waitloop
+)
+
+echo.
+set /p choice="firefox? (y/n): "
+
+if /i "%choice%"=="y" (
+    echo.
+    start "" "Z:\FirefoxPortable\FirefoxPortable.exe"
+) else (
+    echo.
+    echo aight bro
+    timeout /t 3 >nul
+)
