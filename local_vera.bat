402ca907a5e9cf4e1f67a43a7663e72fc80b2942
Ben Piñon <dabenz8293@gmail.com>
2026-05-07 12:08:48 +0800

    vera for local

---
+++ local_vera.bat
@@ -0,0 +1,30 @@
+@echo off
+
+echo grabbing flash drive letter...
+echo.
+timeout /t 2 >nul
+
+echo starting veracrypt...
+timeout /t 2 >nul
+
+cd /d "C:\Program Files\VeraCrypt"
+
+"C:\Program Files\VeraCrypt\VeraCrypt.exe" /v "%flshlttr%\drives\rgd_flgn" /l Z /a /q
+
+if not exist Z:\ (
+    timeout /t 1 >nul
+    goto waitloop
+)
+timeout /t 2 >nul
+start Z:
+echo.
+set /p choice="firefox? (y/n): "
+
+if /i "%choice%"=="y" (
+    echo.
+    start "" "Z:\FirefoxPortable\FirefoxPortable.exe"
+) else (
+    echo.
+    echo aight bro
+    timeout /t 3 >nul
+)
