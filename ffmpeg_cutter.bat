ea0b86bb3d6d2e061ae1ca607c93e46ccb3587cb
Ben Piñon <dabenz8293@gmail.com>
2026-07-22 22:49:22 +0800

    ffmpeg check

--- ffmpeg_cutter.bat
+++ ffmpeg_cutter.bat
@@ -1,23 +1,32 @@
 @echo off
+setlocal
 
 echo ==========================
 echo      FFmpeg Video Cutter
 echo ==========================
 echo.
 
+where ffmpeg >nul 2>&1
+
+if %errorlevel% neq 0 (
+    echo ffmpeg is not installed
+    installing now
+    winget install -e --id Gyan.FFmpeg
+)
+
 set /p input=Enter input video: 
 set /p output=Enter output file: 
 set /p time=Enter duration: 
 
 echo.
-echo cutting video
+echo Cutting video...
 ffmpeg -i "%input%" -t %time% -c copy "%output%"
 
 echo.
-if %errorlevel%==0 (
-    echo done
+if %errorlevel% equ 0 (
+    echo Done!
 ) else (
-    echo failed
+    echo Failed.
 )
 
 pause
